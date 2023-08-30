e74d01b5c60e82820ebe8793f78028e5b768f552
Will Gerry <william.gerry@outlook.com>
2023-08-30 10:27:55 +0100

    Initial portfolio setup

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar () {
+    <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/profile">Profile</NavLink></li>
+        <li><NavLink to="/projects">Projects</NavLink></li>
+        <li><NavLink to="">Home</NavLink></li>
+        <li><NavLink to="">Home</NavLink></li>
+        <li><NavLink to="">Home</NavLink></li>
+    </ul>
+};
+
+export default Navbar;
