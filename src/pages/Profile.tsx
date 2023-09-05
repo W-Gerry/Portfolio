2de31e73c4dbd9e5dc3c562efba4da4465b8b884
Will Gerry <william.gerry@outlook.com>
2023-09-05 22:08:30 +0100

    Update default styling. Add initial profile section.

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,24 @@
+import profileImage from "../assets/images/profile/profile.jpg";
 
+import "./Profile.scss";
 
 function Profile () {
     return (
         <>
-            <h1>This be the profile page</h1>
+            <h1>Profile</h1>
+            <section className="container">
+                <div className="profile-intro">
+                <img className="profile-intro__image" src={profileImage} />
+                    <div className="profile-intro__content">
+                        <h2 className="profile-intro__content__about-me">
+                            About Me
+                        </h2>
+                        <p className="profile-intro__content__text">
+                            This is  the about me section
+                        </p>
+                    </div>
+                </div>
+            </section>
         </>
     )
 };
