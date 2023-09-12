a5d1c7d3c4818c084bdd39010df4725ac267700e
Will Gerry <william.gerry@outlook.com>
2023-09-12 16:33:18 +0100

    Update to Hash Router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.scss'
 
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/Home'
