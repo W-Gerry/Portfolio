302946a812dbb30f885d3a0744060a0f05304818
Will Gerry <william.gerry@outlook.com>
2023-09-26 13:16:40 +0100

    Add disappear on scroll effect to logo. Fix navbar z-position.

--- src/components/homepage/HomepageLogo.tsx
+++ src/components/homepage/HomepageLogo.tsx
@@ -3,9 +3,27 @@ import "./HomepageLogo.scss";
 import logoLarge from "../../assets/general/Logo.svg"
 import chevronDown from "../../assets/navigation/chevrons_down.svg"
 
+import { useEffect, useState } from "react";
+
 const HomepageLogo = () => {
+  const [visible, setVisible] = useState(true)
+  const setLogoVisibility = () => {
+    if (scrollY > 50) {
+      setVisible(false);
+    } else {
+      setVisible(true);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", setLogoVisibility)
+    return () => {
+      window.removeEventListener("scroll", setLogoVisibility)
+    }
+  }, [])
+
   return (
-    <div className="homepage-logo-wrapper">
+    <div className={`homepage-logo-wrapper ${visible && `Visible`}`}>
       <img 
         className="homepage-logo-wrapper__logo" 
         src={logoLarge}
