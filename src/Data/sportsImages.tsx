5f060ebaa5a554bbbaf6465e4ba31cb4329bfc65
Will Gerry <william.gerry@outlook.com>
2023-09-30 20:12:32 +0100

    Add carousel component for sports images. Requires further styling.

---
+++ src/Data/sportsImages.tsx
@@ -0,0 +1,16 @@
+const sportsImages = [
+  "src/assets/sportsphotos/image_1.jpg",
+  "src/assets/sportsphotos/image_2.jpg",
+  "src/assets/sportsphotos/image_3.jpg",
+  "src/assets/sportsphotos/image_4.jpg",
+  "src/assets/sportsphotos/image_5.jpg",
+  "src/assets/sportsphotos/image_6.jpg",
+  "src/assets/sportsphotos/image_7.jpg",
+  "src/assets/sportsphotos/image_8.jpg",
+  "src/assets/sportsphotos/image_9.jpg",
+  "src/assets/sportsphotos/image_10.jpg",
+  "src/assets/sportsphotos/image_11.jpg",
+  "src/assets/sportsphotos/image_12.jpg",
+];
+
+export default sportsImages;
--- src/components/sportphotocarousel/SportPhotoCarousel.tsx
+++ src/components/sportphotocarousel/SportPhotoCarousel.tsx
@@ -1,12 +1,143 @@
 import "./SportPhotoCarousel.scss";
 
+import sportsImages from "../../data/sportsImages";
+
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
 interface SportPhotoCarouselProps {
   isVisible: boolean;
 }
 
-const SportPhotoCarousel = ( {isVisible} : SportPhotoCarouselProps) => {
+const SportPhotoCarousel = ({ isVisible }: SportPhotoCarouselProps) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [direction, setDirection] = useState("");
+
+  const slideVariants = {
+    hiddenRight: {
+      x: "100%",
+      opacity: 0,
+    },
+    hiddenLeft: {
+      x: "-100%",
+      opacity: 0,
+    },
+    visible: {
+      x: "0",
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+    },
+    exit: {
+      opacity: 0,
+      scale: 0.8,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+  const slidersVariants = {
+    hover: {
+      scale: 1.2,
+      backgroundColor: "#ff00008e",
+    },
+  };
+  const dotsVariants = {
+    initial: {
+      y: 0,
+    },
+    animate: {
+      y: -10,
+      scale: 1.2,
+      transition: { type: "spring", stiffness: 1000, damping: "10" },
+    },
+    hover: {
+      scale: 1.1,
+      transition: { duration: 0.2 },
+    },
+  };
+
+  const handleNext = () => {
+    setDirection("right");
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === sportsImages.length ? 0 : prevIndex + 1
+    );
+  };
+
+  const handlePrevious = () => {
+    setDirection("left");
+
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? sportsImages.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleDotClick = (index : number) => {
+    setDirection(index > currentIndex ? "right" : "left");
+    setCurrentIndex(index);
+  };
+
   return (
-    <p className={`Placeholder ${isVisible && `Placeholder-visible`}`}>Placeholder for carousel</p>
+    <div className={`carousel ${isVisible && `carousel-visible`}`}>
+        <div className="carousel-images">
+        <AnimatePresence>
+          <motion.img
+            key={currentIndex}
+            src={sportsImages[currentIndex]}
+            initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
+            animate="visible"
+            exit="exit"
+            variants={slideVariants}
+          />
+        </AnimatePresence>
+        <div className="slide_direction">
+          <motion.div
+            variants={slidersVariants}
+            whileHover="hover"
+            className="left"
+            onClick={handlePrevious}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              viewBox="0 96 960 960"
+              width="20"
+            >
+              <path d="M400 976 0 576l400-400 56 57-343 343 343 343-56 57Z" />
+            </svg>
+          </motion.div>
+          <motion.div
+            variants={slidersVariants}
+            whileHover="hover"
+            className="right"
+            onClick={handleNext}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="20"
+              viewBox="0 96 960 960"
+              width="20"
+            >
+              <path d="m304 974-56-57 343-343-343-343 56-57 400 400-400 400Z" />
+            </svg>
+          </motion.div>
+        </div>
+      </div>
+      <div className="carousel-indicator">
+        {sportsImages.map((_, index) => (
+          <motion.div
+            key={index}
+            className={`dot ${currentIndex === index ? "active" : ""}`}
+            onClick={() => handleDotClick(index)}
+            initial="initial"
+            animate={currentIndex === index ? "animate" : ""}
+            whileHover="hover"
+            variants={dotsVariants}
+          ></motion.div>
+        ))}
+      </div>
+    </div>
   )
 }
 
