d9acdd17c0f72bcb9b93eeee0cc792270190ddbe
Will Gerry <william.gerry@outlook.com>
2023-10-01 20:56:24 +0100

    Update font styling. Add arrows on project tiles. Add grid layout to projects.

--- src/components/projects/ProjectGallery.tsx
+++ src/components/projects/ProjectGallery.tsx
@@ -1,23 +1,39 @@
 import "./ProjectGallery.scss"
 
 import projects from "../../data/projects";
+import caretDown from "../../assets/navigation/caret_down.svg"
 
-function Projects () {
+import { useState } from "react";
+
+function ProjectGallery () {
+  const [activeArrow, setActiveArrow] = useState()
+  const [showProjectDetails, setShowProjectDetails] = useState(false);
+
+  const displayProjectDetails = () => {
+    setShowProjectDetails(!showProjectDetails);
+  }
+
   return (
-    <div className="project-page-wrapper">
+    <div className="Project-gallery-wrapper">
       <h1>This be the projects page</h1>
-      {projects.map((project) => 
-        <div className="project-item">
-          <img className="project-image" alt={`${project.name} screenshot`} src={`${project.imageUrl}`}/>
-          <div className="project-text">
-            <h2>{project.name}</h2>
-            <p>{project.description}</p>
-            {project.link && <a href={project.link} target="_blank">See this project live</a>}
+      <div className="Project-gallery">
+        {projects.map((project) => 
+          <div className="Project-gallery__item">
+            <img className="Project-gallery__image" alt={`${project.name} screenshot`} src={`${project.imageUrl}`}/>
+            <div className="Project-gallery__text">
+              <h2 className="Project-gallery__text__title">{project.name}</h2>
+              <p className="Project-gallery__text__description">{project.description}</p>
+            </div>
+            <img 
+              className={`Project-gallery__arrow ${showProjectDetails && `Project-gallery__arrow--active`}`}
+              src={caretDown}
+              onClick={() => displayProjectDetails()}
+            />
           </div>
-        </div>
-      )}      
+        )}      
+      </div>
     </div>
   )
 };
 
-export default Projects;
+export default ProjectGallery;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,21 +1,11 @@
 import "./Projects.scss"
 
-import projects from "../data/projects";
+import ProjectGallery from "../components/projects/ProjectGallery";
 
 function Projects () {
   return (
-    <div className="project-page-wrapper">
-      <h1>This be the projects page</h1>
-      {projects.map((project) => 
-        <div className="project-item">
-          <img className="project-image" alt={`${project.name} screenshot`} src={`${project.imageUrl}`}/>
-          <div className="project-text">
-            <h2>{project.name}</h2>
-            <p>{project.description}</p>
-            {project.link && <a href={project.link} target="_blank">See this project live</a>}
-          </div>
-        </div>
-      )}      
+    <div className="Project-page-wrapper">
+      <ProjectGallery />
     </div>
   )
 };
