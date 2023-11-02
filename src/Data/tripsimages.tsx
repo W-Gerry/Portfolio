c06bfbec4e09af19818a2d0d7422fa3d62c1a04e
Will Gerry <william.gerry@outlook.com>
2023-11-02 12:09:58 +0000

    Update import for all trip images to be rendered live

--- src/Data/tripsimages.tsx
+++ src/Data/tripsimages.tsx
@@ -9,218 +9,244 @@ import image_8_cornwall23 from "/images/trips/cornwall23/image_8.webp"
 
 export const cornwall23Images = [
   {
-    // image:"/images/trips/cornwall23/image_1.webp",
     image: image_1_cornwall23,
     title: "Bude",
     text: "Hello there"
   },
   {
-    // image:"/images/trips/cornwall23/image_2.webp",
     image: image_2_cornwall23,
     title: "Bude",
     text: "Hello there"
   },
   {
-    // image:"/images/trips/cornwall23/image_3.webp",
     image: image_3_cornwall23,
     title: "Bude",
     text: "Hello there"
   },
   {
-    // image:"/images/trips/cornwall23/image_4.webp",
     image: image_4_cornwall23,
     title: "Bude",
     text: "Hello there"
   },
   {
-    // image:"/images/trips/cornwall23/image_5.webp",
     image: image_5_cornwall23,
     title: "Bude",
     text: "Hello there"
   },
   {
-    // image:"/images/trips/cornwall23/image_6.webp",
     image: image_6_cornwall23,
     title: "Bude",
     text: "Hello there"
   },
   {
-    // image:"/images/trips/cornwall23/image_7.webp",
     image: image_7_cornwall23,
     title: "Bude",
     text: "Hello there"
   },
   {
-    // image:"/images/trips/cornwall23/image_8.webp",
     image: image_8_cornwall23,
     title: "Bude",
     text: "Hello there"
   },
 ];
 
+import image_1_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_2_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_3_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_4_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_5_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_6_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_7_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_8_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_9_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_10_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_11_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_12_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_13_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_14_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_15_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_16_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_17_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_18_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_19_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_20_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_21_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_22_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_23_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_24_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_25_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_26_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_27_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_28_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_29_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_30_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_31_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_32_costarica22 from "/images/trips/costarica22/image_1.webp"
+import image_33_costarica22 from "/images/trips/costarica22/image_1.webp"
+
 export const costarica22Images = [
   {
-    image:"/images/trips/costarica22/image_1.webp",
+    image: image_1_costarica22,
     title: "Home Base - Turrialba",
     text: "All of us Volunteer Managers and the field base team at Casa Blanca in Turrialba waiting for the volunteers to arrive."
   },
   {
-    image:"/images/trips/costarica22/image_2.webp",
+    image: image_2_costarica22,
     title: "View from my room",
     text: "And by room, I mean the bed covered with a mosquito net I moved on to the patio outside because the weather was too amazing not to."
   },
   {
-    image:"/images/trips/costarica22/image_3.webp",
+    image: image_3_costarica22,
     title: "Project 1 - Cabecar School",
     text: "It was a pretty brutal 5 hour hike with all our kit to the bottom of the valley where the school was but the views were definitely worth it."
   },
   {
-    image:"/images/trips/costarica22/image_4.webp",
+    image: image_4_costarica22,
     title: "Washed away",
     text: "And when we made it to the bottom of the valley there were multiple river crossings to contend with."
   },
   {
-    image:"/images/trips/costarica22/image_5.webp",
+    image: image_5_costarica22,
     title: "Comfier than it looks",
     text: "My sleeping arrangements for the three and a half weeks we sepnt in the school building a new classroom."
   },
   {
-    image:"/images/trips/costarica22/image_6.webp",
+    image: image_6_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_7.webp",
+    image: image_7_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_8.webp",
+    image: image_8_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_9.webp",
+    image: image_9_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_10.webp",
+    image: image_10_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_11.webp",
+    image: image_11_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_12.webp",
+    image: image_12_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_13.webp",
+    image: image_13_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_14.webp",
+    image: image_14_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_15.webp",
+    image: image_15_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_16.webp",
+    image: image_16_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_17.webp",
+    image: image_17_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_18.webp",
+    image: image_18_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_19.webp",
+    image: image_19_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_20.webp",
+    image: image_20_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_21.webp",
+    image: image_21_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_22.webp",
+    image: image_22_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_23.webp",
+    image: image_23_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_24.webp",
+    image: image_24_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_25.webp",
+    image: image_25_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_26.webp",
+    image: image_26_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_27.webp",
+    image: image_27_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_28.webp",
+    image: image_28_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_29.webp",
+    image: image_29_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_30.webp",
+    image: image_30_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_31.webp",
+    image: image_31_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_32.webp",
+    image: image_32_costarica22,
     title: "Bude",
     text: "Hello there"
   },
   {
-    image:"/images/trips/costarica22/image_33.webp",
+    image: image_33_costarica22,
     title: "Bude",
     text: "Hello there"
   },  
